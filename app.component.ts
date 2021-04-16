efea5195e628813cbcacb299e505d7080308b04a
mnaudet73 <martin.audet@inavista.com>
2021-04-16 14:31:38 -0400

    1 file modified

--- app.component.ts
+++ app.component.ts
@@ -1,22 +1,24 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { DataManager, WebApiAdaptor } from '@syncfusion/ej2-data';
-import { GridComponent } from '@syncfusion/ej2-angular-grids';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { DataManager, WebApiAdaptor } from "@syncfusion/ej2-data";
+import { GridComponent } from "@syncfusion/ej2-angular-grids";
 
-const SERVICE_URI: string = 'https://inavista-lookbook.azurewebsites.net/';
+const SERVICE_URI: string = "https://inavista-lookbook.azurewebsites.net/";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: 'app.component.html'
+  selector: "app-root",
+  templateUrl: "app.component.html"
 })
 export class AppComponent {
-    public data: DataManager;
-    public pageSettings: Object;
+  public data: DataManager;
+  public pageSettings: Object;
 
-    @ViewChild('grid')
-    public grid: GridComponent;
+  @ViewChild("grid")
+  public grid: GridComponent;
 
-    ngOnInit(): void {
-        this.data = new DataManager({ url: SERVICE_URI + 'api/SoldToBP/GetSoldToBP?pageNumber=1&recPerpage=100', adaptor: new WebApiAdaptor });
-        
-    }
+  ngOnInit(): void {
+    this.data = new DataManager({
+      url: SERVICE_URI + "api/SoldToBP/GetSoldToBP?pageNumber=1&recPerpage=100",
+      adaptor: new WebApiAdaptor()
+    });
+  }
 }
